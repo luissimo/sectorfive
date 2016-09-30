225b8ef4613486215d9127e96e0efbe960ece5ef
luissimo <khijbar@gmail.com>
2016-09-30 21:36:26 +0200

    ship movements initiated

---
+++ SectorFive_1/player.rb
@@ -0,0 +1,26 @@
+require 'gosu'
+
+class Player < Gosu::Window
+
+  def initialize(window)
+  	@x = 200
+  	@y = 200
+  	@angle = 0
+  	@image = Gosu::Image.new('images/ship.png')
+  end
+  
+  def draw
+  	@image.draw_rot(@x, @y, 1, @angle)
+  end
+
+  def turn_right
+  	@angle += 3
+  end
+
+  def turn_left
+  	@angle -= 3
+  end
+  
+end
+
+
